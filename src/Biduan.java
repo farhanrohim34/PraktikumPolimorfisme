a4896252de9be3d77a0343f6a7d451c9185b8cdd
farhanrohim34 <68888131+farhanrohim34@users.noreply.github.com>
2022-11-15 20:53:37 +0700

    Add files via upload

---
+++ src/Biduan.java
@@ -0,0 +1,6 @@
+public class Biduan extends Singer {
+  @Override
+  public void perform() {
+    System.out.println("Beraksi di atas panggung, bernyayi dengan merdu, dengan cengkok melayu");
+  }
+}
---
+++ src/Singer.java
@@ -0,0 +1,6 @@
+public class Singer extends Musician {
+  @Override
+  public void perform() {
+    System.out.println("Beraksi di atas panggung, bernyayi dengan merdu");
+  }
+}
